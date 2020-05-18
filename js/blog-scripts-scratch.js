c0ffa09fe42333b9c402a57406e7c51b140d954a
sabrinamiller05 <64619724+sabrinamiller05@users.noreply.github.com>
2020-05-18 10:01:04 -0700

    Added files

---
+++ js/blog-scripts-scratch.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+  $('.post-wrapper').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    nextArrow: $('.next'),
+    prevArrow: $('.prev'),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+  });
+
+});
